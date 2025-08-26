7ab6d743b6ebbb45135b5f87afce2cd202db9852
ppllnnyy <1779167439@qq.com>
2025-08-26 20:14:28 +0800

    Add files via upload

---
+++ 字符串拼接.py
@@ -0,0 +1,6 @@
+print("我是" + "人")
+name = "人"
+print("我是" + name)
+# 无法和非字符串拼接
+tel = "123456";
+print("我是" + name +"电话：" +tel)
---
+++ 字符串格式化.py
@@ -0,0 +1,10 @@
+class_num = 22
+avg_salary = 16666
+# %s将数字转换字符串
+message = "我现在 %s 岁 身上有%s" % (class_num, avg_salary)
+print(message)
+# %d 将内容转换成整数 %f将内容转换成浮点数
+avg_salary = 12333
+avg = 114.51
+message = "我现在有 %d 还有%f" % (avg_salary,avg)
+print(message)
