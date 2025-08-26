4f767c3fc3277118767053b5ffb3d7a7571f339e
ppllnnyy <1779167439@qq.com>
2025-08-26 17:36:18 +0800

    Add files via upload

---
+++ 数据类型转换.py
@@ -0,0 +1,18 @@
+#转换浮点数
+
+#整数转换浮点数
+float_num = float(11)
+print(type(float_num),float_num)
+
+#浮点转换整数数
+int_num = int(11.345)
+print(type(int_num),int_num)
+
+# int(x)  将x转换为整数
+# float(x) 将x转换为浮点数
+# str(x) 将x转换为字符串
+
+# 只要将括号内的x套上“”，即可以转换为字符串，但字符串不可以随意转换成数字，必须只有数字的字符串
+
+# 浮点数转换整数会丢失精度
+
