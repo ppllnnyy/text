d8792c527874b156c1b2cd75c5f4495bbc684550
ppllnnyy <1779167439@qq.com>
2025-08-31 23:34:20 +0800

    Add files via upload

---
+++ 布尔类型和比较运算符.py
@@ -0,0 +1,19 @@
+# 定义变量储存布尔类型的数据
+bool_1 = True
+bool_2 = False
+print(f"bool_1的变量内容是：{bool_1},类型是：{type(bool_1)}")
+print(f"bool_2的变量内容是：{bool_2},类型是：{type(bool_2)}")
+# 比较运算符的相等比较
+# == , != > < >= <=
+num1 = 10
+num2 = 10
+print(f"10 == 10 的结果是：{num1 == num2}")
+
+num1 = 10
+num2 = 15
+print(f"10 != 15的结果是：{num1 != num2}")
+
+print(f"10 > 15的结果是: {num1 > num2}")
+num1 = 10
+num2 = 10
+print(f"10 >= 10的结果是: {num1 >= num2}")
