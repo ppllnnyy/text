eae175a5ef51f50a0de7a7a9a17d494d7fcabe11
ppllnnyy <1779167439@qq.com>
2025-09-01 21:11:33 +0800

    Add files via upload

---
+++ 猜猜数字小练习.py
@@ -0,0 +1,10 @@
+print("猜猜数字")
+
+if int(input("输入第一次数字")) == 10:
+    print("恭喜你答对了")
+elif int(input("第二次")) == 10:
+    print("恭喜你答对了")
+elif int(input("第三次")) == 10:
+    print("恭喜答对了")
+else:
+    print("全部答错")
