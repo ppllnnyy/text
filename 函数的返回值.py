6929b7d4d8461bfc52bb111f49534d9334bdd992
ppllnnyy <1779167439@qq.com>
2025-09-04 16:39:27 +0800

    Add files via upload
    Signed-off-by: ppllnnyy <1779167439@qq.com>

---
+++ 函数的返回值.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    result = a + b
+    # 通过返回值，将相加的结果返回给调用者
+    return result
+
+
+r = add(1,2)
+print(r)
+
+"""
+
+def 函数():
+    函数体
+    return 返回值
+    
+变量 = 函数(参数)
+
+"""
+
+def add(a,b):
+    result = a + b
+    return result
+
