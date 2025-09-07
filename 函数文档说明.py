0eddf2691a539c197ff5ae52692461142b955a6c
ppllnnyy <1779167439@qq.com>
2025-09-07 17:19:11 +0800

    Add files via upload
    Signed-off-by: ppllnnyy <1779167439@qq.com>

---
+++ 函数文档说明.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    """
+    add函数可以接收2个参数，进行2数相加的功能
+    :param x: 形参x表示相加的其中一个数字
+    :param y: 形参y表示相加的另一个数字
+    :return: 返回值是2数相加的结果
+    """
+    result = x + y
+    print(f"2数相加的结果是：{result}")
+    return result
+# 鼠标移到add显示
+add(1,2)
